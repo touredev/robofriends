d9537515c735352f1d99e5b0b31600b683784cf5
Jean TOURE <touremjean@hotmail.com>
2019-10-02 18:46:27 +0000

    Initial commit

---
+++ src/CardsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+const CardsList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot, i) => {
+        return (
+          <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardsList;
