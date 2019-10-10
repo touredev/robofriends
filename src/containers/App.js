e10b155e55cd119800fada8f408371ba583eb629
Jean TOURE <touremjean@hotmail.com>
2019-10-10 16:21:27 +0000

    Components & Styling

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import CardsList from "../components/CardsList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import { users } from "../robots";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    /*fetch("https:jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));*/
+
+    this.setState({ robots: users });
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { robots, searchField } = this.state;
+    if (!robots.length) return <h1>Loading</h1>;
+
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardsList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
